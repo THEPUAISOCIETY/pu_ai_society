f055f6f4828957ccbb92e2e05873f55fe93ef3ce
THEPUAISOCIETY <thepuaisocietyg@gmail.com>
2024-05-23 22:37:04 +0530

    folder structure added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Aim from "./components/Aim/Aim"
+import AnnualEvent from "./components/AnnualEvent/AnnualEvent"
+import Header from "./components/Header/Header"
+import Navbar from "./components/Navbar/Navbar"
+import Vision from "./components/vision/Vision"
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Aim />
+      <Vision />
+      <AnnualEvent />
+    </>
+  )
+}
+
+export default App
