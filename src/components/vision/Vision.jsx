a945c7320eb2eb66caa8920ce313a028f7649132
THEPUAISOCIETY <thepuaisocietyg@gmail.com>
2024-05-23 22:38:37 +0530

    Vision component added

---
+++ src/components/vision/Vision.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from './Vision.module.css'
+
+function Vision() {
+  return (
+    <div className={classes.vision}>
+
+        {/* heading */}
+        <div className={classes.heading}>
+          <p>Vision  of this project</p>
+        </div>
+
+        {/* description */}
+        <div className={classes.description}>
+          <p>To cultivate a dynamic community at The PU AI Society where diversity fuels innovation, knowledge is shared freely, and every member is empowered to harness the transformative potential of Artificial Intelligence for the betterment of society. Through collaboration, education, and ethical practices, we envision a future where AI serves as a force for positive change across all disciplines and sectors, contributing to a more inclusive and sustainable world. We aspire to elevate The PU AI Society to becoming a beacon of innovation and excellence not only within Parul University but also recognized by governmental bodies for our impactful out-of-the-box initiatives, setting a precedent for pioneering work in the field of AI.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Vision
