13a3d9f59a2c4c4517623df85402505fc3fbedbd
Purvi <purvi.v.malviya@gmail.com>
2024-07-03 13:00:25 +0530

    vision section added

--- src/components/vision/Vision.jsx
+++ src/components/vision/Vision.jsx
@@ -4,6 +4,7 @@ import classes from './Vision.module.css'
 function Vision() {
   return (
     <div className={classes.vision}>
+        <div className={classes.mesh}></div>
 
         {/* heading */}
         <div className={classes.heading}>
@@ -14,6 +15,12 @@ function Vision() {
         <div className={classes.description}>
           <p>To cultivate a dynamic community at The PU AI Society where diversity fuels innovation, knowledge is shared freely, and every member is empowered to harness the transformative potential of Artificial Intelligence for the betterment of society. Through collaboration, education, and ethical practices, we envision a future where AI serves as a force for positive change across all disciplines and sectors, contributing to a more inclusive and sustainable world. We aspire to elevate The PU AI Society to becoming a beacon of innovation and excellence not only within Parul University but also recognized by governmental bodies for our impactful out-of-the-box initiatives, setting a precedent for pioneering work in the field of AI.</p>
         </div>
+
+        <div className={classes.blobcontainer}>
+            <div className={classes.blob1}></div>
+            <div className={classes.blob2}></div>
+        </div>
+
     </div>
   )
 }
