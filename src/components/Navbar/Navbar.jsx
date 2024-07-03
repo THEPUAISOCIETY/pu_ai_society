215cd80e8b76d8ddb7de64582aae5e67ae18947a
Purvi <purvi.v.malviya@gmail.com>
2024-07-03 12:59:57 +0530

    Navbar section added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 import classes from './Navbar.module.css'
 import logo from '../../assets/logo.png'
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav>
         {/*  logo */}
         <div className={classes.leftSide}>
             <img src={logo} alt="logo" className={classes.logo} />
-            <p className={classes.title}>The PU AI Society</p>
+            <p className={classes.title}>The PU AI Society</p> 
+            {/* The PU AI Society */}
         </div>
 
         {/* home, students */}
@@ -20,6 +27,24 @@ function Navbar() {
         <div className={classes.rightSide}>
             <button>Join us</button>
         </div>
+        
+        {/* dropdown */}
+        <div className={classes.burger} onClick={toggleMenu}>
+          <div className={classes.line}></div>
+          <div className={classes.line}></div>
+          <div className={classes.line}></div>
+        </div>
+
+        {isOpen && (
+        <div className={classes.dropdownMenu}>
+          <a href="#">Home</a>
+          <a href="#">Students</a>
+          <a href="#">Join Us</a>
+        </div>
+        )}
+        {/* <div className={classes.opt}></div>
+        <div className={classes.opt}></div>
+        <div className={classes.opt}></div> */}
     </nav>
   )
 }
