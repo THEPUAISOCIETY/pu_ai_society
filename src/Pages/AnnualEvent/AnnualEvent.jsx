1a109d7afb4b0c5a8e7912a09567a6e57ae0aa55
hetashridk <hetashrik@gmail.com>
2024-07-03 13:08:07 +0530

    Annual event page added

--- src/Pages/AnnualEvent/AnnualEvent.jsx
+++ src/Pages/AnnualEvent/AnnualEvent.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './AnnualEvent.module.css';
-import icon1 from '../../assets/Date.png'
-import icon2 from '../../assets/Ai.png'
-import icon3 from '../../assets/Mentor.png'
-import icon4 from '../../assets/Diversity.png'
-import icon5 from '../../assets/Prize.png'
-import icon6 from '../../assets/Divergroup.png'
-
-const itemOne = [
-  {imgname: icon1, title: 'Multi-day event with diverse, interdisciplinary student teams'},
-  {imgname: icon2, title: 'Develop innovative AI applications or solutions.'},
-  {imgname: icon3, title: 'Corporate/faculty mentors to guide teams.'},
-  {imgname: icon4, title: 'This diversity fosters creativity and encourages unique perspectives in problem-solving. '},
-  {imgname: icon5, title: 'Prizes for most impactful AI hacks'},
-  {imgname: icon6, title: 'Mandatory to have diversity within each group, ensuring representation from different academic disciplines.'},
-]
+import { itemOne, itemThree, itemTwo } from '../../lib/constant';
 
 // const itemTwo = [
 //   {title: 'This diversity fosters creativity and encourages unique perspectives in problem-solving. '},
 //   {title: 'Prizes for most impactful AI hacks'},
 //   {title: 'Mandatory to have diversity within each group, ensuring representation from different academic disciplines.'},
 // ]
+
+const cards = [itemOne, itemTwo, itemThree];
+
 function AnnualEvent() {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % cards.length);
+    }, 5000); // Change slide every 5 seconds
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={classes.eventsection}>
         <div className={classes.blobcontainer}>
@@ -48,15 +47,15 @@ function AnnualEvent() {
 
             {/* elements */}
             <div className={classes.element}>
-              {itemOne.map((item, index) => (
-                <div className={classes.box_cont} key={index}>
-                  <div className={classes.boximg}>
-                    <img className={classes.icon} src={item.imgname} alt="" />
-                  </div>
-                  <p className={classes.text_cont}>{item.title}</p>
+            {cards[currentSlide].map((item, index) => (
+              <div className={classes.box_cont} key={index}>
+                <div className={classes.boximg}>
+                  <img className={classes.icon} src={item.imgname} alt="" />
                 </div>
-              ))}
-            </div>
+                <p className={classes.text_cont}>{item.title}</p>
+              </div>
+            ))}
+          </div>
 
             {/* elements */}
             {/* <div  className={classes.element}>
@@ -69,6 +68,16 @@ function AnnualEvent() {
           
           </div>
         </div>
+          {/* dots navigation */}
+          <div className={classes.dots}>
+            {cards.map((_, index) => (
+              <span
+                key={index}
+                className={`${classes.dot} ${currentSlide === index ? classes.active : ''}`}
+                onClick={() => setCurrentSlide(index)}
+              ></span>
+            ))}
+          </div>
     </div>
   )
 }
