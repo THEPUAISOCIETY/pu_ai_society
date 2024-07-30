6281a3d28150dab41041d9150aaad54337d7e3d9
Purvi <purvi.v.malviya@gmail.com>
2024-07-31 03:41:56 +0530

    mesh added, footer added, vision modified, fixed other issues

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Footer.module.css';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/footer_logo.png';
 import Location from '../../assets/Location.png';
 import Contact from '../../assets/Contact.png';
 import Mail from '../../assets/Mail.png';
@@ -16,25 +16,43 @@ function Footer() {
     <footer className={classes.footer}>
       <div className={classes.container}>
         <div className={classes.topSection}>
+
           <div className={classes.logoContainer}>
             <img src={logo} alt="PU AI Society Logo" className={classes.logo} />
           </div>
-          <div className={classes.infoContainer}>
-            <div className={classes.infoItem}>
-              <img src={Location} alt="Location" />
-              <p>Parul University</p>
+
+          <div className={classes.right}>
+            <div className={classes.navLinks}>
+              {NAVITEM.map((item, key) => (
+                <Link to={item.link} key={key}>
+                  {item.title}
+                </Link>
+              ))}
             </div>
-            <div className={classes.infoItem}>
-              <img src={Contact} alt="Contact" />
-              <p>+91 6353435262</p>
-            </div>
-            <div className={classes.infoItem}>
-              <img src={Mail} alt="Mail" />
-              <p>
-                <a href="mailto:thepuaisocietyg@gmail.com">thepuaisocietyg@gmail.com</a>
-              </p>
+            
+            <div className={classes.infoContainer}>
+              <div className={classes.infoItem}>
+                <img src={Location} alt="Location" />
+                <p>Parul University</p>
+              </div>
+              <div className={classes.infoItem}>
+                <img src={Contact} alt="Contact" />
+                <p>+91 6353435262</p>
+              </div>
+              <div className={classes.infoItem}>
+                <img src={Mail} alt="Mail" />
+                <p>
+                  <a href="mailto:thepuaisocietyg@gmail.com">thepuaisocietyg@gmail.com</a>
+                </p>
+              </div>
             </div>
           </div>
+ 
+
+        </div>
+
+        <div className={classes.bottomSection}>
+
           <div className={classes.socialContainer}>
             <p>Follow Us</p>
             <div className={classes.socialIcons}>
@@ -52,15 +70,7 @@ function Footer() {
               </a>
             </div>
           </div>
-        </div>
-        <div className={classes.bottomSection}>
-          <div className={classes.navLinks}>
-            {NAVITEM.map((item, key) => (
-              <Link to={item.link} key={key}>
-                {item.title}
-              </Link>
-            ))}
-          </div>
+          
           <div className={classes.credit}>
             <p>© 2024 PU AI Society</p>
             <a href="https://ritikavarma.com/" target="_blank" rel="noopener noreferrer">
